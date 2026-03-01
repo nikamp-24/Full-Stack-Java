3dd5c7ab2ab71ec5ce3f52e5d5ba36b6fbb69a83
Pooja Pratap Nikam <146447841+nikamp-24@users.noreply.github.com>
2026-03-01 18:55:24 +0000

    variables and datatypes

---
+++ JS/Day_02.js
@@ -0,0 +1,33 @@
+// Data types in js 
+
+"use strict"; // treat all js code as a newer version
+
+// alert(3+3) // we are using nodejs not browser so will give error 
+
+console.log(3 
+    +3 ) // not good practice code redability should be high 
+
+console.log("Pooja")
+
+let name = "Pooja"
+let age = 18 
+let isLoggedIn = false
+let state = null
+
+
+// Primitive Data Types
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null  => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof"Pooja");
+console.log(typeof undefined);
+console.log(typeof null);
