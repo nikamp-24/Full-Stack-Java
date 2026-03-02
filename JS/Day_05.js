43eb2a593ea72ef9a6914cbdbe24f1a0dd55713a
Pooja Pratap Nikam <146447841+nikamp-24@users.noreply.github.com>
2026-03-02 16:21:21 +0000

    datatypes and variables

---
+++ JS/Day_05.js
@@ -0,0 +1,18 @@
+// 7 types : String , Number , Boolean , Null, Undefined , Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+
+let userEmail;
+
+const id = Symbol('123')
+
+const anotherId = Symbol('123')
+
+// Reference (Non Primitive )
+
+// Array , Objects, Functions
+
