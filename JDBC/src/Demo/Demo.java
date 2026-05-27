22a7b333f7c626c8ae756b3deafe1eacedd21905
nikamp-24 <nikamp870@gmail.com>
2026-05-27 21:40:41 +0530

    Testing GitHub sync

--- JDBC/src/Demo/Demo.java
+++ JDBC/src/Demo/Demo.java
@@ -10,5 +10,6 @@ public static void main(String[] args) {
 //	 Welcome to JDBC tutorial
 	System.out.println("Hello JDBC");
 	System.out.println("Hello JDBC");
+	System.out.println("GitHub Sync Test");
 }
 }
